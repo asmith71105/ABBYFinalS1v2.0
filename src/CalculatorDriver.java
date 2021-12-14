7ce03a85834c0d68d53b1505ab6fa160e7800c11
Maddison <mtranchau1@gmail.com>
2021-12-13 19:13:50 -0800

    A

--- src/CalculatorDriver.java
+++ src/CalculatorDriver.java
@@ -4,12 +4,22 @@ public class CalculatorDriver
     {
         Expression expression1 = new Expression(2, "+", 21);
         Expression expression2 = new Expression(8, "-", 45);
-        Expression expression3 = new Expression(44, "*", 9);
+        Expression expression3 = new Expression(44, "+", 9);
 
-        int answer1 = MethodCreation.Calculate(expression1);
-        int answer2 = MethodCreation.Calculate(expression2);
+//        int answer1 = MethodCreation.Calculate(expression1);
+//        int answer2 = MethodCreation.Calculate(expression2);
         int answer3 = MethodCreation.Calculate(expression3);
 
-        System.out.println(answer1);
+//        System.out.println(answer1);
+//        System.out.println(expression2.getValue1());
+//        System.out.println(answer2);
+        System.out.println(expression3.getValue1());
+        System.out.println(expression3.getOperator());
+        System.out.println(expression3.getValue2());
+
+        System.out.println(SetUp.calcAdd(44, "+", 9));
+        System.out.println(SetUp.calcAdd(2, "+", 21));
+        System.out.println(SetUp.calcAdd(8, "-", 45));
+
     }
 }
--- src/SetUp.java
+++ src/SetUp.java
@@ -2,30 +2,37 @@
 
 public class SetUp
 {
+    public static int firstValue = Expression.getValue1();
+    public static int secondValue = Expression.getValue2();
+    public static int answer = 0;
     // Methods ---------------------------------------------------------------------------------------------------------
 
     //This method will perform the addition process of a calculator
     public static int calcAdd(int value1, String operator, int value2)
     {
-        return Expression.value1 + Expression.value2;
+        answer = firstValue + secondValue;
+        return answer;
     }
 
     //This method will perform the subtraction process of a calculator
     public static int calcSub(int value1, String operator, int value2)
     {
-        return Expression.value1 - Expression.value2;
+        answer = firstValue - secondValue;
+        return answer;
     }
 
     //This method will perform the multiplication process of a calculator
     public static int calcMult(int value1, String operator, int value2)
     {
-        return Expression.value1 * Expression.value2;
+        answer = firstValue * secondValue;
+        return answer;
     }
 
     //This method will perform the division process of a calculator
     public static int calcDivi(int value1, String operator, int value2)
     {
-        return Expression.value1 / Expression.value2;
+        answer = firstValue / secondValue;
+        return answer;
     }
 
     //This method will display an error for expressions that do not produce a valid sum/difference/product/quotient,
